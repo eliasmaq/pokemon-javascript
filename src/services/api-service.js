9c1fb942f5c307496bb18574187c441bed38d91b
sebastianterleira <seb.terleira1204@gmail.com>
2023-03-16 21:09:22 -0500

    Codigo Spaguety

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,9 +1,24 @@
-const link = "https://api.pokemontcg.io/v2/cards/"
+const cardsContainer = document.getElementById('root');
 
-async function apiService (link, ...options) {
-    let response = await fetch(link);
-    let data = await response.json();
-    console.log(data)
-}
+fetch('https://api.pokemontcg.io/v2/cards?pageSize=50')
+  .then(response => response.json())
+  .then(data => {
+    // procesar la respuesta aquí
+    const cards = data.data;
 
-apiService(link)
+    // crear el contenido HTML para mostrar las cartas
+    let cardsHTML = '';
+    cards.forEach(card => {
+      cardsHTML += `
+				<div class="root">
+					<div class="card">
+          	<img src="${card.images.small}" alt="${card.name}" class="card__item">
+					</div>
+        </div>
+      `;
+    });
+
+    // agregar el contenido HTML al elemento 'cards' en el DOM
+    cardsContainer.innerHTML = cardsHTML;
+  })
+  .catch(error => console.error(error));
